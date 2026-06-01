e0788a769632f7475669f5457301d2ef12d703c2
Omkar Jaybhaye <omkarjaybhaye06@gmail.com>
2026-06-01 16:29:57 +0530

    basics-js-theory

---
+++ 1_basics/1_Theory.js
@@ -0,0 +1,16 @@
+console.log("JavaScript");
+/*
+JavaScript is a high-level, interpreted, dynamically typed programming language primarily used to create interactive and dynamic web applications.
+JS runs in web browsers and on servers through environments like Node.js. 
+JS supports object-oriented, functional, and event-driven programming.
+JS is a versatile language that allows developers to manipulate the Document Object Model (DOM), handle events, and communicate with servers using technologies like AJAX.
+JS is syncronous (executes code in sequential order)by default but can handle asynchronous operations using callbacks, promises, and async/await.
+JS is single-threaded, meaning it can only execute one task at a time, but it can manage multiple tasks through its event loop and asynchronous programming features.
+JS events are used for and as follows: 1. User interactions: Clicks, mouse movements, keyboard input, etc. 2. Form submissions: Handling form data and validation.
+JS properties: alert, console, document, window, etc. are used to access and manipulate various aspects of the web page and browser environment.
+Variables: Containers for storing data values. In JavaScript, you can declare variables using var, let, or const. with let and const being block-scoped and var being function-scoped.
+Functions: Reusable blocks of code that perform a specific task. Functions can be defined using function declarations, function expressions, or arrow functions.
+Objects: Collections of properties and methods that represent real-world entities. Objects can be created using object literals, constructor functions, or classes. Propertys are key-value pairs that define the characteristics of an object, while methods are functions that perform actions on the object.
+Arrays: Ordered collections of values that can hold multiple data types. Arrays are created using square brackets [] and can be manipulated using various array methods.
+Event Handling: JavaScript allows you to respond to user interactions and other events using event listeners. You can attach event listeners to HTML elements to execute code when specific events occur, such as clicks, mouse movements, or form submissions.
+*/
